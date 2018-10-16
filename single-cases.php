94a2c57e0ca17ffea41921fe6e0bdbfe4e6a0f44
Vladimir Samojlov <v-moi@ya.ru>
2018-10-16 14:31:38 +0300

    edit single cases

--- single-cases.php
+++ single-cases.php
@@ -1,1 +1,21 @@
-<?php the_title(); ?>
+<?php get_header(); ?>
+
+<div style="background: url(<?php bloginfo('template_url'); ?>/img/bg-bottom.svg) center bottom/105% no-repeat;">
+	<?php get_template_part( 'blocks/page-top', 'default' ); ?>
+</div>
+
+<div class="bg-white">
+	<div class="container py-5">
+		<div class="row">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class="col-md-12">
+				<?php the_content(); ?>
+			</div>
+			<?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
