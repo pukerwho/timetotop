7212e3993cdeb626e88409dd038a6e7960c3111f
Vladimir Samojlov <v-moi@ya.ru>
2018-11-02 15:44:25 +0200

    some edit form

--- footer.php
+++ footer.php
@@ -102,7 +102,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <?php echo do_shortcode('[multi-step-form id="1"]'); ?>
+            <?php echo do_shortcode('[contact-form-7 id="189" title="Multi-steps"]'); ?>
           </div>
         </div>
       </div>
