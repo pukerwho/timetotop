b8fdbaf852c5494e848d3926359211bca32242e1
Vladimir Samojlov <v-moi@ya.ru>
2018-11-02 15:47:45 +0200

    some edit form

--- footer.php
+++ footer.php
@@ -93,7 +93,7 @@
     </div>
     <!-- quiz Modal -->
     <div class="modal fade" id="quizModal" tabindex="-1" role="dialog" aria-labelledby="quizLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Простой опрос</h5>
