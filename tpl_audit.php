56376b9545229d390d9da7548e0a2790fbcf81f6
Vladimir Samojlov <v-moi@ya.ru>
2018-11-08 15:51:10 +0200

    add tpl audit

---
+++ tpl_audit.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: Аудит
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="tpl_audit">
+	<div style="background: url(<?php bloginfo('template_url'); ?>/img/bg-bottom.svg) center bottom/105% no-repeat;">
+		<?php get_template_part( 'blocks/page-top', 'default' ); ?>
+	</div>
+	<div class="bg-white">
+		<div class="container py-5">
+			<div class="row justify-content-center">
+				<div class="col-md-10">
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<div class="lead">
+							<?php the_content(); ?>	
+						</div>
+					<?php endwhile; else: ?>
+						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+					<?php endif; ?>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-10">
+					<?php echo do_shortcode('[contact-form-7 id="200" title="Бесплатный аудит сайта"]'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
