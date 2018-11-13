f185b0228a6a568a58736a75194a3414c11127ed
Vladimir Samojlov <v-moi@ya.ru>
2018-11-13 21:44:53 +0200

    edit src to data-src

--- blocks/portfolio.php
+++ blocks/portfolio.php
@@ -12,7 +12,7 @@
 				<div class="portfolio__item">
 					<a href="<?php echo rwmb_meta( 'meta-portfolio-link' ); ?>">
 						<div class="portfolio__item-absolute"></div>
-						<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="portfolio__item-bg">
+						<img data-src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="portfolio__item-bg">
 						<div class="portfolio__item-logo">
 							<?php 
 								rwmb_the_value( 'meta-portfolio-logo', array( 'size' => 'large' ) );
