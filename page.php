cbe7d25764c1074b00e3bd08c6945427e22ea2b9
Vladimir Samojlov <v-moi@ya.ru>
2018-11-15 11:11:43 +0200

    edit page

--- page.php
+++ page.php
@@ -4,4 +4,18 @@
 	<?php get_template_part( 'blocks/page-top', 'default' ); ?>
 </div>
 
+<div class="bg-white">
+	<div class="container py-5">
+		<div class="row">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class="col-md-12 lead">
+				<?php the_content(); ?>
+			</div>
+			<?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
+
 <?php get_footer(); ?>
