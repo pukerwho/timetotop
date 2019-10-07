e49606fc0721c76d595bc5df8121650284c6e30e
Vladimir Samojlov <v-moi@ya.ru>
2019-10-07 18:18:38 +0300

    edit some

--- page.php
+++ page.php
@@ -8,18 +8,18 @@
 	<?php get_template_part( 'blocks/page-top', 'default' ); ?>
 </div>
 
-<div class="bg-white">
-	<div class="container py-5">
-		<div class="row">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<div class="col-md-12 lead">
-				<?php the_content(); ?>
-			</div>
-			<?php endwhile; else: ?>
-				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-			<?php endif; ?>
+<div class="container py-5">
+	<div class="row">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<div class="col-md-12 lead">
+			<?php the_content(); ?>
 		</div>
+		<?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+		<?php endif; ?>
 	</div>
 </div>
+
+<?php get_template_part('blocks/home-services') ?>
 
 <?php get_footer(); ?>
